bdbc83b2e1760c9ed771719f7b9ff6283a3ae53a
K Kiran Kumar <113544336+kiran150502@users.noreply.github.com>
2022-09-17 09:58:40 +0530

    Add files via upload

---
+++ strings_basics.py
@@ -0,0 +1,38 @@
+print('hello'* 5) # hellohellohellohellohello
+print('hello ' * 5) # hello hello hello hello hello   
+print('he\n'*5,end='')
+
+print('hi' * 3.4)
+
+print('hi' * 'b')
+
+print('hi' + 4)
+print('hi' + 3.7)
+
+print('hi'+'bye')
+print('hi '+'bye')
+print('hi'+' bye')
+print('hi'+''+'bye')
+print('hi'+' '+'bye')
+print('hi'+'                 '+'bye')
+
+print('hi'+'-'+'5')
+
+print('hello' 'bye')
+print('hello' ,'bye')
+print('hello'+'  '+'bye')
+print('hello'+'  '+str(7))
+
+print('Today is thursday time is 4:32pm')
+print("Today is thursday time is 4:32pm")
+print('''Today is thursday time is 4:32pm''')
+print("""Today is thursday time is 4:32pm""")
+print('Today'+' '+'is'+' '+'thursday'+' '+'time'+' '+'is'+' '+'4:32pm')
+
+print(1,2,3,4,5,6,7)
+print('1'+'  '+'2'+'  '+'3')
+
+print(1,2,3,4,5,sep='')
+print(1,2,3,4,5,sep=' ')
+print(1,2,3,4,5,sep='  ')
+print(1,2,3,4,5,sep='-')
